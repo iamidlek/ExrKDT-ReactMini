3e271e823f433d6acdd2e34f7fa6989b343924f1
iamidlek <iamidlek@naver.com>
2021-12-18 18:18:17 +0900

    feat: re render when url changed

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,15 +1,27 @@
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import Box from "@mui/material/Box";
-import { useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const Routes = () => {
   const navigate = useNavigate();
+
+  const { pathname } = useLocation();
+
   const [value, setValue] = useState(0);
+
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
+
+  useEffect(() => {
+    if (pathname === "/") setValue(0);
+    if (pathname === "/search") setValue(1);
+    if (pathname.includes("/product")) setValue(2);
+    if (pathname === "/cart") setValue(3);
+    if (pathname === "/order") setValue(4);
+  }, [pathname]);
   return (
     <Box sx={{ borderBottom: 1, borderColor: "#ccc", marginBottom: "30px" }}>
       <Tabs value={value} onChange={handleChange}>
