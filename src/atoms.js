876054e4cadad5ad75e0944bd0c147eb2887d570
iamidlek <iamidlek@naver.com>
2021-12-21 10:20:58 +0900

    set sample email

--- src/atoms.js
+++ src/atoms.js
@@ -3,7 +3,7 @@ import { atom } from "recoil";
 export const userAuth = atom({
   key: "userAuth",
   default: {
-    user_email: "sample가입절차를건너뛴email",
+    user_email: "ddd@ddd.com",
     user_name: "없음",
     user_confirm: "Y", // ?
     user_password: "", // 암호화된 정보
