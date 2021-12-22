43355ba797b911c3681f8f50cf67f41cfed0c2e0
iamidlek <iamidlek@naver.com>
2021-12-22 12:59:26 +0900

    로그인 기능 전까지 아이디 직접 부여

--- src/atoms.js
+++ src/atoms.js
@@ -4,6 +4,7 @@ export const userAuth = atom({
   key: "userAuth",
   default: {
     user_email: "ddd@ddd.com",
+    // user_email: "",
     user_name: "없음",
     user_confirm: "Y", // ?
     user_password: "", // 암호화된 정보
