118b4f2c4dfb3084ea5e19d4f44b414810440345
iamidlek <iamidlek@naver.com>
2021-12-22 13:59:08 +0900

    feat: add chart

--- src/components/Order.js
+++ src/components/Order.js
@@ -23,8 +23,6 @@ const Order = () => {
   const [orderList, setOrderList] = useState([]);
   const [datePick, setDatePick] = useState([null, null]);
 
-  const [toggle, setToggle] = useState(false);
-
   const auth = useRecoilValue(userAuth);
   const navigate = useNavigate();
 
@@ -67,7 +65,6 @@ const Order = () => {
             variant="outlined"
             onClick={() => {
               pageNation(1);
-              setToggle(false);
             }}
             style={{
               width: "100%",
@@ -82,133 +79,121 @@ const Order = () => {
           <Button
             variant="outlined"
             color="success"
-            onClick={() => setToggle(true)}
             style={{
               width: "100%",
               height: "50px",
               fontSize: "18px",
             }}
+            onClick={() => navigate("/order/chart")}
           >
             차트 보기
           </Button>
         </Grid>
         <Grid item xs={7.2}></Grid>
-        {toggle ? (
-          <>
-            <div>chart</div>
-          </>
-        ) : (
-          <>
-            <Grid item xs={5}>
-              <LocalizationProvider dateAdapter={DateAdapter}>
-                <DateRangePicker
-                  startText="시작일"
-                  endText="종료일"
-                  calendars={1}
-                  value={datePick}
-                  onChange={(newValue) => {
-                    setDatePick(newValue);
-                  }}
-                  renderInput={(startProps, endProps) => (
-                    <>
-                      <TextField autoComplete="off" {...startProps} />
-                      <Box sx={{ mx: 1 }}> - </Box>
-                      <TextField autoComplete="off" {...endProps} />
-                    </>
-                  )}
-                />
-              </LocalizationProvider>
-            </Grid>
-          </>
-        )}
-      </Grid>
-      {!toggle && (
-        <>
-          <Grid container style={{ textAlign: "center", marginBottom: "16px" }}>
-            <Grid item xs={2}>
-              <ListItemText primary="결제일" />
-            </Grid>
-            <Grid item xs={2}>
-              <ListItemText primary="수취인" />
-            </Grid>
-            <Grid item xs={2}>
-              <ListItemText primary="결제주" />
-            </Grid>
-            <Grid item xs={2}>
-              <ListItemText primary="결제액" />
-            </Grid>
-            <Grid item xs={3}>
-              <ListItemText primary="결제정보" />
-            </Grid>
-            <Grid item xs={1}>
-              <ListItemText primary="상세보기" />
-            </Grid>
-          </Grid>
-          <Box>
-            <List style={{ borderTop: "1px solid #ccc" }}>
-              {orderList?.map((item) => (
-                <ListItem
-                  disablePadding
-                  key={item.order_id}
-                  style={{ borderBottom: "1px solid #ccc" }}
-                >
-                  <Grid
-                    container
-                    style={{ textAlign: "center", alignItems: "center" }}
-                  >
-                    <Grid item xs={2}>
-                      <ListItemText primary={item.insert_date.slice(0, 10)} />
-                    </Grid>
-                    <Grid item xs={2}>
-                      <ListItemText primary={item.receive_user} />
-                    </Grid>
-                    <Grid item xs={2}>
-                      <ListItemText primary={item.card_user} />
-                    </Grid>
-                    <Grid item xs={2}>
-                      <ListItemText
-                        primary={item.total_price.toLocaleString() + "원"}
-                      />
-                    </Grid>
-                    <Grid item xs={3}>
-                      <ListItemText primary={item.cart_dv} />
-                      <ListItemText
-                        primary={item.card_number1 + "-xxxx-xxxx-xxxx"}
-                      />
-                    </Grid>
-                    <Grid item xs={1}>
-                      <Button
-                        variant="outlined"
-                        onClick={() =>
-                          navigate(`/order/${item.order_id}`, {
-                            state: { order_id: item.order_id },
-                          })
-                        }
-                      >
-                        상세
-                      </Button>
-                    </Grid>
-                  </Grid>
-                </ListItem>
-              ))}
-            </List>
-          </Box>
-          {!datePick[0] && (
-            <Pagination
-              style={{
-                width: "100%",
-                display: "flex",
-                justifyContent: "center",
-                marginTop: 30,
+        <Grid item xs={5}>
+          <LocalizationProvider dateAdapter={DateAdapter}>
+            <DateRangePicker
+              startText="시작일"
+              endText="종료일"
+              calendars={1}
+              value={datePick}
+              onChange={(newValue) => {
+                setDatePick(newValue);
               }}
-              count={count ? Math.ceil(count / 10) : 1}
-              color="secondary"
-              showFirstButton
-              showLastButton
-              onChange={(event, page) => pageNation(page)}
+              renderInput={(startProps, endProps) => (
+                <>
+                  <TextField autoComplete="off" {...startProps} />
+                  <Box sx={{ mx: 1 }}> - </Box>
+                  <TextField autoComplete="off" {...endProps} />
+                </>
+              )}
             />
-          )}
-        </>
+          </LocalizationProvider>
+        </Grid>
+      </Grid>
+      <Grid container style={{ textAlign: "center", marginBottom: "16px" }}>
+        <Grid item xs={2}>
+          <ListItemText primary="결제일" />
+        </Grid>
+        <Grid item xs={2}>
+          <ListItemText primary="수취인" />
+        </Grid>
+        <Grid item xs={2}>
+          <ListItemText primary="결제주" />
+        </Grid>
+        <Grid item xs={2}>
+          <ListItemText primary="결제액" />
+        </Grid>
+        <Grid item xs={3}>
+          <ListItemText primary="결제정보" />
+        </Grid>
+        <Grid item xs={1}>
+          <ListItemText primary="상세보기" />
+        </Grid>
+      </Grid>
+      <Box>
+        <List style={{ borderTop: "1px solid #ccc" }}>
+          {orderList?.map((item) => (
+            <ListItem
+              disablePadding
+              key={item.order_id}
+              style={{ borderBottom: "1px solid #ccc" }}
+            >
+              <Grid
+                container
+                style={{ textAlign: "center", alignItems: "center" }}
+              >
+                <Grid item xs={2}>
+                  <ListItemText primary={item.insert_date.slice(0, 10)} />
+                </Grid>
+                <Grid item xs={2}>
+                  <ListItemText primary={item.receive_user} />
+                </Grid>
+                <Grid item xs={2}>
+                  <ListItemText primary={item.card_user} />
+                </Grid>
+                <Grid item xs={2}>
+                  <ListItemText
+                    primary={item.total_price.toLocaleString() + "원"}
+                  />
+                </Grid>
+                <Grid item xs={3}>
+                  <ListItemText primary={item.cart_dv} />
+                  <ListItemText
+                    primary={item.card_number1 + "-xxxx-xxxx-xxxx"}
+                  />
+                </Grid>
+                <Grid item xs={1}>
+                  <Button
+                    variant="outlined"
+                    onClick={() =>
+                      navigate(`/order/${item.order_id}`, {
+                        state: { order_id: item.order_id },
+                      })
+                    }
+                  >
+                    상세
+                  </Button>
+                </Grid>
+              </Grid>
+            </ListItem>
+          ))}
+        </List>
+      </Box>
+      {!datePick[0] && (
+        <Pagination
+          style={{
+            width: "100%",
+            display: "flex",
+            justifyContent: "center",
+            marginTop: 30,
+          }}
+          count={count ? Math.ceil(count / 10) : 1}
+          color="secondary"
+          showFirstButton
+          showLastButton
+          onChange={(event, page) => pageNation(page)}
+        />
       )}
     </>
   );
